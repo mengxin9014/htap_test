1903a3c5858bb102d15e4d0eb7a719e78cace588
mengxin <tregoldmeng@gmail.com>
2021-12-01 19:24:58 +0800

    delete tidb after testing

--- scripts/run_htap_test.sh
+++ scripts/run_htap_test.sh
@@ -3,6 +3,7 @@ tcctl testbed create -f htap_test/config/tidb.yaml -r http://rms.pingcap.net:300
 
 pd_host=$(KUBECONFIG=kubeconfig.yml  kubectl get pod/htap-test-pd-0 -owide | grep htap-test-pd-0 | awk '{print $6}')
 tidb_host=$(KUBECONFIG=kubeconfig.yml  kubectl get pod/htap-test-tidb-0 -owide | grep htap-test-tidb-0 | awk '{print $6}')
+namespace=$(tcctl testbed list -r http://rms.pingcap.net:30007 | grep htap-test-tidb |awk '{print $1}')
 
 KUBECONFIG=kubeconfig.yml kubectl exec -it htap-test-tiflash-0 -- mkdir /htap_test
 
@@ -15,3 +16,5 @@ KUBECONFIG=kubeconfig.yml kubectl exec -it htap-test-tiflash-0 -- sh /htap_test/
 mkdir record/
 KUBECONFIG=kubeconfig.yml kubectl cp htap-test-tiflash-0:/htap_test/benchbase/record/ch_benchmark_test.txt record/ch_benchmark_test.txt
 KUBECONFIG=kubeconfig.yml kubectl cp htap-test-tiflash-0:/htap_test/benchbase/record/ch_benchmark_small_query_test.txt record/ch_benchmark_small_query_test.txt
+
+tcctl testbed delete ${namespace} -r http://rms.pingcap.net:30007
