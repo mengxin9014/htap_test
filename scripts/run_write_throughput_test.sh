6070403927e27e432c21d18d49cf6742a958cc97
mengxin <tregoldmeng@gmail.com>
2021-12-07 17:49:41 +0800

    modify incorrect dir

--- scripts/run_write_throughput_test.sh
+++ scripts/run_write_throughput_test.sh
@@ -25,12 +25,12 @@
 function gen_yaml() {
     local kv=$1
     local flash=$2
-    rm -rf write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
-    cat write_throughput_test/config/write_thoughput_test_tidb.yaml > write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
-    cat write_throughput_test/config/tikv.yaml >> write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
-    echo "        replica: $kv" >> write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
-    cat write_throughput_test/config/tiflash.yaml >> write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
-    echo "        replica: $flash" >> write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    rm -rf htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    cat htap_test/config/write_thoughput_test_tidb.yaml > htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    cat htap_test/config/tikv.yaml >> htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    echo "        replica: $kv" >> htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    cat htap_test/config/tiflash.yaml >> htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
+    echo "        replica: $flash" >> htap_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml
 }
 
 threads="1 2 4"
@@ -53,7 +53,7 @@ do
       namespace=$(tcctl testbed list -r http://rms.pingcap.net:30007 | grep write-throughput-test-tidb |awk '{print $1}')
     done
 
-    tcctl testbed create -f write_throughput_test/config/write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml -r http://rms.pingcap.net:30007
+    tcctl testbed create -f htap_test/config/write_throughput_test/config/write_thoughput_test_tidb_${kv}_${flash}.yaml -r http://rms.pingcap.net:30007
 
     namespace=$(tcctl testbed list -r http://rms.pingcap.net:30007 | grep write-throughput-test-tidb |awk '{print $1}')
     pd_host=$(KUBECONFIG=kubeconfig.yml  kubectl -n ${namespace} get pod/write-throughput-test-pd-0 -owide | grep write-throughput-test-pd-0 | awk '{print $6}')
@@ -62,12 +62,12 @@ do
 
     KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} exec -it write-throughput-test-tiflash-0 -- mkdir /write_throughput_test
 
-    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp write_throughput_test/scripts/init_write_throughput_test.sh write-throughput-test-tiflash-0:/write_throughput_test
-    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp write_throughput_test/scripts/start_write_throughput_test.sh write-throughput-test-tiflash-0:/write_throughput_test
+    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp htap_test/scripts/init_write_throughput_test.sh write-throughput-test-tiflash-0:/write_throughput_test
+    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp htap_test/scripts/start_write_throughput_test.sh write-throughput-test-tiflash-0:/write_throughput_test
 
     KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} exec -it write-throughput-test-tiflash-0 -- sh /write_throughput_test/init_write_throughput_test.sh
 
-    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp write_throughput_test/table_statics/benchbase_table_static.tar.gz write-throughput-test-tiflash-0:/write_throughput_test/benchbase
+    KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} cp htap_test/table_statics/benchbase_table_static.tar.gz write-throughput-test-tiflash-0:/write_throughput_test/benchbase
     KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} exec -it write-throughput-test-tiflash-0 -- tar zxvf /write_throughput_test/benchbase/benchbase_table_static.tar.gz -C /write_throughput_test/benchbase
 
     KUBECONFIG=kubeconfig.yml kubectl -n ${namespace} exec -it write-throughput-test-tiflash-0 -- sh /write_throughput_test/start_write_throughput_test.sh ${tidb_host} ${pd_host} ${thread}
