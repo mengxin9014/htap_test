46b82ec1fcdb2353875f2ab12cf0457eb418edd5
mengxin <tregoldmeng@gmail.com>
2021-12-08 13:14:54 +0800

    add duration

--- scripts/start_write_throughput_test.sh
+++ scripts/start_write_throughput_test.sh
@@ -3,4 +3,4 @@ source /etc/profile
 tidb_host=$1
 pd_host=$2
 thread=$3
-ticat write_throughput.run h=${tidb_host} p=4000 pp=2379 ph=${pd_host} sf=1 cp=/htap_test/benchbase  br_storage='s3\://patrick/baseline/sysbench-1T-1ktable-3000krow' t=${thread} tn=1000 ts=3000000 : write_throughput.record cp=/htap_test/benchbase
+ticat write_throughput.run h=${tidb_host} p=4000 pp=2379 ph=${pd_host} d=300 sf=1 cp=/htap_test/benchbase  br_storage='s3\://patrick/baseline/sysbench-1T-1ktable-3000krow' t=${thread} tn=1000 ts=3000000 : write_throughput.record cp=/htap_test/benchbase
